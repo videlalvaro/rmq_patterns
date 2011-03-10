8e1f36d745e281acd57550aad78f2609ad1b74cc
Alvaro Videla <videlalvaro@gmail.com>
2011-03-10 20:30:25 +0100

    test consumer for debugging purposes

--- src/amqp_test.erl
+++ src/amqp_test.erl
@@ -8,11 +8,15 @@ start(Opts) ->
     {ok, Connection} = amqp_connection:start(network, #amqp_params{}),
     ProxyEx = <<"char_count">>,
     RpcExchange = <<"char_count_server">>,
-    Fun = undefined,
-    amqp_smart_proxy:start(Connection, ProxyEx, RpcExchange, Fun, Opts).
+    ControlExchange = <<"control">>,
+    Pid = amqp_smart_proxy:start([Connection, ProxyEx, RpcExchange, ControlExchange, interval_ms(), Opts]),
+    io:format("Server started with Pid: ~p~n", [Pid]),
+    Pid.
 
 start() ->
     start([]).
 
 start_debug() ->
     start([{debug, [trace]}]).
+    
+interval_ms() -> 60000.
