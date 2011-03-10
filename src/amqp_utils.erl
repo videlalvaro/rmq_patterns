ea5a894fbae56fddd5da4b192c0ded68389d1f24
Alvaro Videla <videlalvaro@gmail.com>
2011-03-10 21:17:28 +0100

    adds stop_consumers/2 and stop_consumer/2 functions

--- src/amqp_utils.erl
+++ src/amqp_utils.erl
@@ -1,6 +1,8 @@
 -module(amqp_utils).
 
--export([init_controlled_consumer/3]).
+-export([init_controlled_consumer/3, stop_consumer/2, stop_consumers/2]).
+
+-include("amqp_client.hrl").
 
 init_controlled_consumer(Channel, ControlExchange, ControlRKey) ->
     #'queue.declare_ok'{queue = ControlQ} 
@@ -13,3 +15,17 @@ init_controlled_consumer(Channel, ControlExchange, ControlRKey) ->
         amqp_channel:subscribe(Channel, #'basic.consume'{queue = ControlQ, no_ack = true}, self()),
     
     {ok, ControlCTag}.
+
+stop_consumer(Consumer, Channel) ->
+    stop_consumers([Consumer], Channel).
+    
+stop_consumers([], _Channel) ->
+    ok;
+stop_consumers([CTag|T], Channel) ->
+    case CTag of
+        <<"">> -> ok;
+        _ ->
+            #'basic.cancel_ok'{} =
+                amqp_channel:call(Channel, #'basic.cancel'{consumer_tag = CTag})
+    end,
+    stop_consumers(T, Channel).
