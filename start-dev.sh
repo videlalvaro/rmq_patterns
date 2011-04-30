aabee3a883329ad03f82846499b4c1b911e20f06
Alvaro Videla <videlalvaro@gmail.com>
2011-04-30 12:31:01 +0200

    improved script for REPL start up

--- start-dev.sh
+++ start-dev.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# ./start-dev.php <config_file_name>
-
 cd `dirname $0`
-exec erl -pa $PWD/ebin -pa $PWD/deps/amqp_client/include/rabbit_common/ebin \
--pa $PWD/deps/amqp_client/ebin  \
--sname amqp_consumer \
-# -s rmq_patterns -boot start_sasl
+exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin \
+-sname rmq_patterns_$1 \
