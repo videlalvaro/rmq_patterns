def49cb891b82ba68fee9fd082cbd01aa9788ed9
Alvaro Videla <videlalvaro@gmail.com>
2011-05-11 00:01:11 +0200

    new utility function for publishing messages

--- src/misc.erl
+++ src/misc.erl
@@ -2,7 +2,11 @@
 
 -include("amqp_client.hrl").
 
--export([declare_exchanges/1, demo_callback/2, word_count_callback/2, word_reverse_callback/2]).
+-export([declare_exchanges/1,
+         demo_callback/2,
+         word_count_callback/2,
+         word_reverse_callback/2,
+         publish_msg/3]).
 
 declare_exchanges(Exchanges) ->
     {ok, Connection} = amqp_connection:start(network, #amqp_params{}),
@@ -28,3 +32,14 @@ word_count_callback(_Channel, #amqp_msg{payload = Msg}) ->
 word_reverse_callback(_Channel, #amqp_msg{payload = Msg}) ->
   Words = lists:reverse(string:tokens(binary_to_list(Msg), " ")),
   io:format("Reversed Words: ~p~n", [Words]).
+
+publish_msg(Exchange, Msg, RKey) ->
+    {ok, Connection} = amqp_connection:start(network, #amqp_params{}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+
+    Publish = #'basic.publish'{exchange = Exchange, routing_key = RKey},
+    amqp_channel:call(Channel, Publish, #amqp_msg{payload = term_to_binary(Msg)}),
+
+    amqp_channel:close(Channel),
+    amqp_connection:close(Connection),
+    ok.
